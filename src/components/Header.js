949f49c4256d93c3ffd2682b422e089d19f8f566
uranov14 <102876402+uranov14@users.noreply.github.com>
2022-08-17 13:38:10 +0300

    Change Header and Section1

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,14 +17,14 @@ const Header = () => {
 
   return (
     <>
-        <header className='absolute flex items-center justify-between px-5 py-8 w-full'>
-            <div>
+        <header className='absolute flex items-center justify-between px-5 md:px-12 py-8 w-full'>
+            <div className='md:pl-12 md:ml-12'>
                 <img src={logo} alt="logo"/>
             </div>
 
             {isOpen && (
                 <nav className='absolute w-9/12 ml-16 mt-56 md:relative 
-                md:mt-0 md:w-auto md:ml-0'>
+                md:mt-0 md:w-auto md:ml-0 md:pr-12 md:mr-5'>
                 <ul className='bg-white shadow-2xl rounded-2xl py-3 
                 md:bg-transparent md:shadow-none md:rounded-none md:py-0 md:flex'>
                     <li className='py-3 border-b border-gray-200 px-5 cursor-pointer 
--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import mastercraft from '../images/logo-mastercraft.svg'
 import bookmark from '../images/icon-bookmark.svg'
+import bookmarkActive from '../images/icon-bookmark-active.svg'
 
 const Section1 = () => {
+  const [isMark, setIsMark] = useState(false)
   return (
     <>
         <section className='relative bg-white mx-5 px-5 pb-10 rounded-lg 
         -mt-20 border-2 lg:w-1/2 lg:mx-auto'>
             <img src={mastercraft} alt="" className='mastercraft-logo'/>
             <div className='pt-10 pb-3'>
-                <h2 className='text-center font-bold text-xl pb-2'>Mastercraft Bamboo Monitor Riser</h2>
+                <h2 className='title text-center pb-2'>
+                    Mastercraft Bamboo Monitor Riser
+                </h2>
                 <p className='text-center'>
                     A beautiful & handcrafted monitor stand to reduce neck and eye strain.
                 </p>
             </div>
 
-            <div className='flex justify-between'>
+            <div className='flex justify-between pt-5'>
                 <button className='btn rounded-full py-1 px-6 text-white'>
                     Back this project
                 </button>
-                <img src={bookmark} alt=""/>
+                {!isMark ? (
+                <div className='flex items-center bg-gray-200 rounded-full cursor-pointer' onClick={() => setIsMark(!isMark)}>
+                    <img src={bookmark} alt=""/>
+                    <span className='hidden md:flex text-gray-500 font-bold px-4'>
+                        Bookmark
+                    </span>
+                </div>
+                ) : (
+                <div className='flex items-center bg-gray-200 rounded-full cursor-pointer' onClick={() => setIsMark(!isMark)}>
+                    <img src={bookmarkActive} alt=""/>
+                    <span className='hidden md:flex text-green-700 font-bold px-4'>
+                        Bookmarked
+                    </span>
+                </div>
+                )}
             </div>
         </section>
     </>
