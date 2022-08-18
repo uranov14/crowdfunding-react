2436cd24a29796f5801523ef850bfa6c7692e0af
uranov14 <102876402+uranov14@users.noreply.github.com>
2022-08-18 15:10:26 +0300

    Add icon-close-modal-active.svg

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Pledges from './Pledges'
 import closeModal from '../images/icon-close-modal.svg'
+import closeModalActive from '../images/icon-close-modal-active.svg'
 import toggleModal from '../toggleModal'
 
 const Modal = () => {
+    const [isHover, setIsHover] = useState(false)
+
   return (
     <>
-        <section id='modal' className='hidden bg-white mx-5 mt-5 p-5 rounded-lg border-2'>
-            <div className='flex justify-between'>
-                <div>
-                    <h4 className='font-bold text-xl'>Back this project</h4>
-                    <p className='my-5'>
-                        Want to support us in bringing Mastercraft Bamboo 
-                        Monitor Riser out in the world?
-                    </p>
+        <main  id='modal' className='hidden absolute flex justify-center w-full'>
+            <section className='bg-white p-5 my-44 rounded-lg border-2 w-1/2'>
+                <div className='flex justify-between'>
+                    <div className='my-4'>
+                        <h4 className='font-bold text-xl'>Back this project</h4>
+                        <p className='my-2'>
+                            Want to support us in bringing Mastercraft Bamboo 
+                            Monitor Riser out in the world?
+                        </p>
+                    </div>
+
+                    {!isHover ? 
+                    <div>
+                        <img src={closeModal} alt="" className='w-6 cursor-pointer'
+                        onMouseOver={ () => setIsHover(!isHover) } 
+                        onClick={ toggleModal } />
+                    </div>
+                    : 
+                    <div>
+                        <img src={closeModalActive} alt="" className='w-6 cursor-pointer'
+                        onMouseOut={() => setIsHover(!isHover) }  
+                        onClick={ toggleModal } />
+                    </div>
+                    }
                 </div>
-                <div className='mt-2'>
-                    <img src={closeModal} alt="" className='w-6 cursor-pointer' 
-                    onClick={ toggleModal }/>
-                </div>
-            </div>
-            <Pledges />
-        </section>
+                <Pledges />
+            </section>
+        </main>
     </>
   )
 }
